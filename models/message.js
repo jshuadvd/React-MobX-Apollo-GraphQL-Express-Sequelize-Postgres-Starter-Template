9fccebff6a75538ca341b937323aef6b080c78f9
jshuadvd <jshuadvd@icloud.com>
2018-06-24 22:14:21 -0700

    feature(BackEnd) Updating models to define underscored for the db)

--- models/message.js
+++ models/message.js
@@ -1,27 +1,23 @@
 export default (sequelize, DataTypes) => {
-	const Message = sequelize.define(
-		'message',
-		{
-			text: DataTypes.STRING
-		},
-		{ underscored: true }
-	)
+	const Message = sequelize.define('message', {
+		text: DataTypes.STRING
+	})
 
 	Message.associate = models => {
 		// 1:M
 		Message.belongsTo(models.Channel, {
-			foreignKey: { 
-				name: 'channelId', 
-				field: 'channel_id' 
+			foreignKey: {
+				name: 'channelId',
+				field: 'channel_id'
 			}
 		}),
-			// 1:M
-			Message.belongsTo(models.User, {
-				foreignKey: { 
-					name: 'userId', 
-					field: 'user_id' 
-				}
-			})
+		// 1:M
+		Message.belongsTo(models.User, {
+			foreignKey: {
+				name: 'userId',
+				field: 'user_id'
+			}
+		})
 	}
 
 	return Message
