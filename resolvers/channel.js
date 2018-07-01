5ff7a428be960f3fe44135e5f14ce86b05996b87
jshuadvd <jshuadvd@icloud.com>
2018-06-30 23:01:19 -0700

    feature(BackEnd) adding uschannel resolvers o schema for db table

--- resolvers/channel.js
+++ resolvers/channel.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+	Mutation: {
+		createChannel: async (parent, args. {models}) => {
+			try {
+				await models.Channel.create(args)
+				return true
+			} catch (e) {
+				console.log("Error: ", e);
+				return false
+			}
+		}
+	}
+};
