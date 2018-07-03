84a143e3b2edd1c7eeecb66841aad3f1aeec2770
jshuadvd <jshuadvd@icloud.com>
2018-07-02 23:34:16 -0700

    feature(BackEnd) adding usmessage resolvers to schema for db table

--- resolvers/message.js
+++ resolvers/message.js
@@ -2,7 +2,7 @@ export default {
 	Mutation: {
 		createMessage: async (parent, args, { models, user }) => {
 			try {
-				await models.Message.create({ ...args, owner: user.id });
+				await models.Message.create({ ...args, userId: user.id });
 				return true;
 			} catch (e) {
 				console.log('ERROR: ', e);
