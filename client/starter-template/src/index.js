9d8c8e5ca014e74eebc1ee8ddbdd76f8bb3a5bd1
jshuadvd <jshuadvd@icloud.com>
2018-07-06 00:07:52 -0700

    feature(BackEnd) adAdded React Router + Apollo

--- client/starter-template/src/index.js
+++ client/starter-template/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { ApolloClient, creatNetworkInterface, ApolloProvider } from 'react-apollo';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import Views from './views';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = <Views />;
+
+ReactDOM.render(App, document.getElementById('root'));
 registerServiceWorker();
