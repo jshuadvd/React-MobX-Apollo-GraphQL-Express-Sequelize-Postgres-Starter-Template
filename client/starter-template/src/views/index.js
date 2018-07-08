1f7ec7195e9600f36090b59f396b2e323a459fff
jshuadvd <jshuadvd@icloud.com>
2018-07-08 00:39:38 -0700

    feature(BackEnd) adAdded ApReact Router

--- client/starter-template/src/views/index.js
+++ client/starter-template/src/views/index.js
@@ -8,20 +8,28 @@ import {
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to Joshua David</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-			</div>
-		);
-	}
-}
+import Home from './Home'
 
-export default App;
+// class App extends Component {
+// 	render() {
+// 		return (
+// 			<div className="App">
+// 				<header className="App-header">
+// 					<img src={logo} className="App-logo" alt="logo" />
+// 					<h1 className="App-title">Welcome to Joshua David</h1>
+// 				</header>
+// 				<p className="App-intro">
+// 					To get started, edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 			</div>
+// 		);
+// 	}
+// }
+
+export default () => {
+	<BrowserRouter>
+		<Switch>
+			<Route path="/" exact component={ Home } />
+		</Switch>
+	</BrowserRouter>
+}
